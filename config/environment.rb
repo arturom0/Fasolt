696b6091836e44211d969fb3d570950c8f8ad9de
Arturo Martinez <arturo.martinez@pearson.com>
2009-10-13 14:19:01 -0500

    Added session key

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file
+# Be sure to restart your server when you modify this file∑
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
@@ -38,4 +38,5 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  config.action_controller.session = { :key => "_fasolt_session", :secret => " iqwehacjnsfu3kdjfasdfadk" }
 end
